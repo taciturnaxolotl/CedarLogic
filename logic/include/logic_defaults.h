28b8400d61b03d7e32a9768462dab46a81e096e2
Kieran Klukas <me@dunkirk.sh>
2026-02-19 19:24:19 -0500

    bug: fix logs overloading the system

--- logic/include/logic_defaults.h
+++ logic/include/logic_defaults.h
@@ -20,7 +20,7 @@
 // Warning message macro:
 #ifndef _PRODUCTION_
 #define WARNING(message) \
-	do { *logiclog << "Warning: " << message << std::endl << std::flush; } while (0);
+	do { *logiclog << "Warning: " << message << "\n"; } while (0);
 #else
 #define WARNING(message) \
 	do { } while (0);
